2c0bd10f0e9d5cff522cc252bf88239237e998dd
Peter Dumbari <dummytwiz@gmail.com>
2023-09-05 16:52:40 +0100

    added integration and n+1 issues fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(comments: :author).find(params[:id])
     @current = current_user
     @comment = Comment.new
     @comments = @post.comments
